b86c18b5c028bf918f8e73c2717b27b2bbcd9dbd
sonichy <sonichy@163.com>
2019-12-11 19:09:01 +0800

    first commit

---
+++ mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <Qt3DExtras/Qt3DWindow>
+#include <Qt3DCore/QEntity>
+#include <Qt3DExtras/QPhongMaterial>
+#include <Qt3DRender/QCamera>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    Qt3DExtras::Qt3DWindow *window;
+    Qt3DCore::QEntity *rootEntity;
+    Qt3DExtras::QPhongMaterial *material;
+    Qt3DRender::QCamera *camera;
+    QString filename="";
+    QWidget *widget3d;
+    QVector3D vector3D_camera;
+    bool isStop;
+    void drawLine(const QVector3D &start, const QVector3D &end, const QColor &color, Qt3DCore::QEntity *_rootEntity);
+
+private slots:
+    void on_actionRender_triggered();
+    void on_actionReduce_triggered();
+    void on_actionIncrease_triggered();
+    void on_actionReset_triggered();
+    void on_actionRotateLeft_triggered();
+    void on_actionAutoRotateLeft_triggered();
+    void on_actionRotateRight_triggered();
+    void on_action_export_triggered();
+
+};
+
+#endif // MAINWINDOW_H
